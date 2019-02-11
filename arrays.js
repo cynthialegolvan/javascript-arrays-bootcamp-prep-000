17a03b6146e5d335af24274a4ed8bd69ebe29131
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-11 23:25:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,5 +8,5 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
   return chocolateBars
 }
 function addElementToEndOfArray(chocolateBars, candyString) {
-  return 
+  
 }
