b5a4cb871f2e220400453602853ef350eff02f8e
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-11 23:51:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,5 +22,6 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
   return chocolateBars
 }
 function removeElementFromBeginningOfArray(chocolateBars) {
-  
+  chocolateBars.slice(1)
+  return chocolateBars
 }
